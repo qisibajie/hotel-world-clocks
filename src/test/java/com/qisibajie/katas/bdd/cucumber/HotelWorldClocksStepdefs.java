a4a38f2e7b410da6de60619b430235cab7bd9313
qisibajie <qisibajie@163.com>
2017-12-11 22:36:13 +0800

    add test

---
+++ src/test/java/com/qisibajie/katas/bdd/cucumber/HotelWorldClocksStepdefs.java
@@ -0,0 +1,58 @@
+package com.qisibajie.katas.bdd.cucumber;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class HotelWorldClocksStepdefs {
+
+    @Given("^a hotel lobby has five clocks$")
+    public void a_hotel_lobby_has_five_clocks() throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+
+    @Given("^\"([^\"]*)\" is in the DST period$")
+    public void is_in_the_DST_period(String arg1) throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+
+    @Given("^\"([^\"]*)\" does not use the DST$")
+    public void does_not_use_the_DST(String arg1) throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+
+    @When("^I adjust the incorrect time of the clock of \"([^\"]*)\" to be (\\d+)$")
+    public void I_adjust_the_incorrect_time_of_the_clock_of_to_be(String arg1, int arg2) throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+
+    @Then("^the time of the clock of \"([^\"]*)\" should be (\\d+)$")
+    public void the_time_of_the_clock_of_should_be(String arg1, int arg2) throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+
+    @Given("^the DST period of \"([^\"]*)\" ends$")
+    public void the_DST_period_of_ends(String arg1) throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+
+    @Given("^the DST period of \"([^\"]*)\" does not end$")
+    public void the_DST_period_of_does_not_end(String arg1) throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+
+    @When("^I adjust the time of the clock of \"([^\"]*)\" to be (\\d+) due to the end of DST$")
+    public void I_adjust_the_time_of_the_clock_of_to_be_due_to_the_end_of_DST(String arg1, int arg2) throws Throwable {
+        // Express the Regexp above with the code you wish you had
+        throw new PendingException();
+    }
+
+}
--- src/test/java/com/qisibajie/katas/bdd/cucumber/RunCukesTest.java
+++ src/test/java/com/qisibajie/katas/bdd/cucumber/RunCukesTest.java
@@ -4,6 +4,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@Cucumber.Options(format = {"pretty", "html:target/cucumber-html-report", "json-pretty:target/cucumber-report.json"})
+@Cucumber.Options(format = {"pretty", "html:target/cucumber-html-report", "json-pretty:target/cucumber-report.json"}
+                        , features = "src/test/resources")
 public class RunCukesTest {
 }
