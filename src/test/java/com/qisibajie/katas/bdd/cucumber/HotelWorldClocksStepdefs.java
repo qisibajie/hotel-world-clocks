ea5f52c08f0afccf0b38fe443bfd71ce2fef1712
qisibajie <qisibajie@163.com>
2017-12-11 23:20:21 +0800

    refine the parameter name

--- src/test/java/com/qisibajie/katas/bdd/cucumber/HotelWorldClocksStepdefs.java
+++ src/test/java/com/qisibajie/katas/bdd/cucumber/HotelWorldClocksStepdefs.java
@@ -14,43 +14,43 @@ public class HotelWorldClocksStepdefs {
     }
 
     @Given("^\"([^\"]*)\" is in the DST period$")
-    public void is_in_the_DST_period(String arg1) throws Throwable {
+    public void is_in_the_DST_period(String cityName) throws Throwable {
         // Express the Regexp above with the code you wish you had
         throw new PendingException();
     }
 
     @Given("^\"([^\"]*)\" does not use the DST$")
-    public void does_not_use_the_DST(String arg1) throws Throwable {
+    public void does_not_use_the_DST(String cityName) throws Throwable {
         // Express the Regexp above with the code you wish you had
         throw new PendingException();
     }
 
     @When("^I adjust the incorrect time of the clock of \"([^\"]*)\" to be (\\d+)$")
-    public void I_adjust_the_incorrect_time_of_the_clock_of_to_be(String arg1, int arg2) throws Throwable {
+    public void I_adjust_the_incorrect_time_of_the_clock_of_to_be(String cityName, int hour) throws Throwable {
         // Express the Regexp above with the code you wish you had
         throw new PendingException();
     }
 
     @Then("^the time of the clock of \"([^\"]*)\" should be (\\d+)$")
-    public void the_time_of_the_clock_of_should_be(String arg1, int arg2) throws Throwable {
+    public void the_time_of_the_clock_of_should_be(String cityName, int expectedHour) throws Throwable {
         // Express the Regexp above with the code you wish you had
         throw new PendingException();
     }
 
     @Given("^the DST period of \"([^\"]*)\" ends$")
-    public void the_DST_period_of_ends(String arg1) throws Throwable {
+    public void the_DST_period_of_ends(String cityName) throws Throwable {
         // Express the Regexp above with the code you wish you had
         throw new PendingException();
     }
 
     @Given("^the DST period of \"([^\"]*)\" does not end$")
-    public void the_DST_period_of_does_not_end(String arg1) throws Throwable {
+    public void the_DST_period_of_does_not_end(String cityName) throws Throwable {
         // Express the Regexp above with the code you wish you had
         throw new PendingException();
     }
 
     @When("^I adjust the time of the clock of \"([^\"]*)\" to be (\\d+) due to the end of DST$")
-    public void I_adjust_the_time_of_the_clock_of_to_be_due_to_the_end_of_DST(String arg1, int arg2) throws Throwable {
+    public void I_adjust_the_time_of_the_clock_of_to_be_due_to_the_end_of_DST(String cityName, int hour) throws Throwable {
         // Express the Regexp above with the code you wish you had
         throw new PendingException();
     }
